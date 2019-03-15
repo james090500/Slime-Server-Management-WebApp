044f1ecf722cd55dcc66162ef9336bbd838fb4da
James Harrison <james@fasttortoise.co.uk>
2019-03-15 11:05:48 +0000

    Added some work to homepage

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,26 @@
 @extends('layouts/default')
 
 @section('content')
-  <h1>Yeet</h1>
+  <div class="box text-center">
+    <h1>Your Server Stats</h1>
+    <div class="row">
+      <div class="col">
+        <i class="fas fa-microchip fa-5x"></i>
+        <h1>19.43 TPS</h1>
+      </div>
+      <div class="col">
+        <i class="fas fa-memory fa-5x"></i>
+        <h1>1.5GB</h1>
+      </div>
+      <div class="col">
+        <i class="fas fa-gamepad fa-5x"></i>
+        <h1>20/50</h1>
+      </div>
+    </div>
+  </div>
+  <div class="box">
+    <div class="row text-center">
+
+    </div>
+  </div>
 @stop
