37923723858217dbfa2ffd673a4005d50abcc477
james090500 <james@fasttortoise.co.uk>
2019-03-28 16:50:21 +0000

    Work on improving log files.

--- resources/views/pages/console.blade.php
+++ resources/views/pages/console.blade.php
@@ -1,66 +1,66 @@
 @extends('layouts/default')
 
 @section('content')
-  <div id="logfile" class="box">
-[18:20:14] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: rip x2
-[18:20:17] [Server thread/INFO]: GUEST » Navoko: lol XD
-[18:20:27] [Craft Scheduler Thread - 1943/INFO]: * mthrs20 is now AFK
-[18:20:29] [Server thread/INFO]: RESPECTED » BOBSTER: ripx7548345734593875
-[18:20:51] [Server thread/INFO]: Navoko issued server command: /rtp
-[18:21:08] [Server thread/INFO]: TealKatt just looted a Zombie Head!
-[18:21:10] [Server thread/INFO]: madxy_ drowned
-[18:21:12] [Server thread/INFO]: PLAYER » IntoHeaven: come on just give me this head already!
-[18:21:25] [Server thread/INFO]: RESPECTED » BOBSTER: just give up
-[18:21:25] [Server thread/INFO]: GUEST » SmacK1776: triggerd
-[18:21:30] [Server thread/INFO]: PLAYER » IntoHeaven: nah
-[18:21:30] [Server thread/INFO]: GUEST » Navoko: does anyone have an iron farm i can use?
-[18:21:32] [Server thread/INFO]: PLAYER » IntoHeaven: never
-[18:21:37] [Server thread/INFO]: Navoko issued server command: /rtp
-[18:21:38] [Server thread/INFO]: RESPECTED » BOBSTER: not me sadly
-[18:21:39] [Server thread/INFO]: Navoko issued server command: /home
-[18:21:43] [Server thread/INFO]: PLAYER » IntoHeaven: iron farm are so broken Lol
-[18:21:48] [Server thread/INFO]: RESPECTED » BOBSTER: i am making a creeper farm though
-[18:21:48] [Server thread/INFO]: madxy_ issued server command: /wild
-[18:21:54] [Server thread/INFO]: GUEST » Navoko: nice
-[18:21:55] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: same
-[18:21:58] [Craft Scheduler Thread - 1900/INFO]: [BuycraftX] Fetching all due players...
-[18:21:59] [Craft Scheduler Thread - 1900/INFO]: [BuycraftX] Fetched due players (6 found).
-[18:22:06] [Server thread/INFO]: Navoko issued server command: /Rtp
-[18:22:14] [Server thread/INFO]: _Bobster_ issued server command: /tpa TheEmeraldFalcon
-[18:22:25] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: why u want to tp to me?
-[18:22:27] [Server thread/INFO]: * mthrs20 is no longer AFK
-[18:22:27] [Server thread/INFO]: SalamanderCmndr issued server command: /warp shops
-[18:22:36] [Server thread/INFO]: RESPECTED » BOBSTER: wanna see the creepre farm
-[18:22:45] [Server thread/INFO]: SalamanderCmndr issued server command: /home
-[18:22:51] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: i havent started properly yet stll getting mats
-[18:23:04] [Server thread/INFO]: RESPECTED » BOBSTER: can i still see
-[18:23:07] [Server thread/INFO]: RESPECTED » BOBSTER: please
-[18:23:21] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: i am at my store not my farm areea
-[18:23:27] [Server thread/INFO]: RESPECTED » BOBSTER: ok
-[18:23:27] [Server thread/INFO]: Navoko issued server command: /home
-[18:23:30] [Server thread/INFO]: SalamanderCmndr issued server command: /home bed
-[18:23:31] [Server thread/INFO]: _Bobster_ issued server command: /warp Market
-[18:23:42] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: it will take a good few days to get resources
-[18:23:52] [Server thread/INFO]: RESPECTED » BOBSTER: i have just started
-[18:24:03] [Server thread/INFO]: mthrs20 issued server command: /timevote day
-[18:24:05] [Server thread/INFO]: RESPECTED » BOBSTER: where is ur store
-[18:24:06] [Server thread/INFO]: itaygoodgod123 issued server command: /tv yes
-[18:24:07] [Server thread/INFO]: SalamanderCmndr issued server command: /tv yes
-[18:24:07] [Server thread/INFO]: CupcakkeV issued server command: /TVYES
-[18:24:07] [Server thread/INFO]: Navoko issued server command: /tv yes
-[18:24:08] [Server thread/INFO]: [CapeCraft] Nessa_Wolfie joined the game
-[18:24:08] [User Authenticator #884/INFO]: UUID of player Nessa_Wolfie is 5624dfe4-6a22-4ffb-bbf4-652e48203d6f
-[18:24:09] [Server thread/INFO]: Nessa_Wolfie[/71.236.170.177:60885] logged in with entity id 5604939 at ([world]-2573.0, 73.0, 8256.0)
-[18:24:10] [Server thread/INFO]: SmacK1776 issued server command: /tv yes
-[18:24:11] [Server thread/INFO]: madxy_ issued server command: /wild
-[18:24:12] [Server thread/INFO]: CupcakkeV issued server command: /tv yes
-[18:24:13] [Server thread/INFO]: PLAYER » IntoHeaven: Nessa_Wolfie
-[18:24:14] [Server thread/INFO]: _Bobster_ issued server command: /tv yes
-[18:24:16] [Server thread/INFO]: PLAYER » IntoHeaven: hello
-[18:24:19] [Server thread/INFO]: SalamanderCmndr issued server command: /home
-[18:24:26] [Server thread/INFO]: TealKatt lost connection: Timed out
-[18:24:26] [Server thread/INFO]: [CapeCraft] TealKatt left the game
-  </div>
+    <ul id="logfile" class="box">
+    <li>[18:20:14] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: rip x2</li>
+    <li>[18:20:17] [Server thread/INFO]: GUEST » Navoko: lol XD</li>
+    <li>[18:20:27] [Craft Scheduler Thread - 1943/INFO]: * mthrs20 is now AFK</li>
+    <li>[18:20:29] [Server thread/INFO]: RESPECTED » BOBSTER: ripx7548345734593875</li>
+    <li>[18:20:51] [Server thread/INFO]: Navoko issued server command: /rtp</li>
+    <li>[18:21:08] [Server thread/INFO]: TealKatt just looted a Zombie Head!</li>
+    <li>[18:21:10] [Server thread/INFO]: madxy_ drowned</li>
+    <li>[18:21:12] [Server thread/INFO]: PLAYER » IntoHeaven: come on just give me this head already!</li>
+    <li>[18:21:25] [Server thread/INFO]: RESPECTED » BOBSTER: just give up</li>
+    <li>[18:21:25] [Server thread/INFO]: GUEST » SmacK1776: triggerd</li>
+    <li>[18:21:30] [Server thread/INFO]: PLAYER » IntoHeaven: nah</li>
+    <li>[18:21:30] [Server thread/INFO]: GUEST » Navoko: does anyone have an iron farm i can use?</li>
+    <li>[18:21:32] [Server thread/INFO]: PLAYER » IntoHeaven: never</li>
+    <li>[18:21:37] [Server thread/INFO]: Navoko issued server command: /rtp</li>
+    <li>[18:21:38] [Server thread/INFO]: RESPECTED » BOBSTER: not me sadly</li>
+    <li>[18:21:39] [Server thread/INFO]: Navoko issued server command: /home</li>
+    <li>[18:21:43] [Server thread/INFO]: PLAYER » IntoHeaven: iron farm are so broken Lol</li>
+    <li>[18:21:48] [Server thread/INFO]: RESPECTED » BOBSTER: i am making a creeper farm though</li>
+    <li>[18:21:48] [Server thread/INFO]: madxy_ issued server command: /wild</li>
+    <li>[18:21:54] [Server thread/INFO]: GUEST » Navoko: nice</li>
+    <li>[18:21:55] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: same</li>
+    <li>[18:21:58] [Craft Scheduler Thread - 1900/INFO]: [BuycraftX] Fetching all due players...</li>
+    <li>[18:21:59] [Craft Scheduler Thread - 1900/INFO]: [BuycraftX] Fetched due players (6 found).</li>
+    <li>[18:22:06] [Server thread/INFO]: Navoko issued server command: /Rtp</li>
+    <li>[18:22:14] [Server thread/INFO]: _Bobster_ issued server command: /tpa TheEmeraldFalcon</li>
+    <li>[18:22:25] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: why u want to tp to me?</li>
+    <li>[18:22:27] [Server thread/INFO]: * mthrs20 is no longer AFK</li>
+    <li>[18:22:27] [Server thread/INFO]: SalamanderCmndr issued server command: /warp shops</li>
+    <li>[18:22:36] [Server thread/INFO]: RESPECTED » BOBSTER: wanna see the creepre farm</li>
+    <li>[18:22:45] [Server thread/INFO]: SalamanderCmndr issued server command: /home</li>
+    <li>[18:22:51] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: i havent started properly yet stll getting mats</li>
+    <li>[18:23:04] [Server thread/INFO]: RESPECTED » BOBSTER: can i still see</li>
+    <li>[18:23:07] [Server thread/INFO]: RESPECTED » BOBSTER: please</li>
+    <li>[18:23:21] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: i am at my store not my farm areea</li>
+    <li>[18:23:27] [Server thread/INFO]: RESPECTED » BOBSTER: ok</li>
+    <li>[18:23:27] [Server thread/INFO]: Navoko issued server command: /home</li>
+    <li>[18:23:30] [Server thread/INFO]: SalamanderCmndr issued server command: /home bed</li>
+    <li>[18:23:31] [Server thread/INFO]: _Bobster_ issued server command: /warp Market</li>
+    <li>[18:23:42] [Server thread/INFO]: PLAYER » TheEmeraldFalcon: it will take a good few days to get resources</li>
+    <li>[18:23:52] [Server thread/INFO]: RESPECTED » BOBSTER: i have just started</li>
+    <li>[18:24:03] [Server thread/INFO]: mthrs20 issued server command: /timevote day</li>
+    <li>[18:24:05] [Server thread/INFO]: RESPECTED » BOBSTER: where is ur store</li>
+    <li>[18:24:06] [Server thread/INFO]: itaygoodgod123 issued server command: /tv yes</li>
+    <li>[18:24:07] [Server thread/INFO]: SalamanderCmndr issued server command: /tv yes</li>
+    <li>[18:24:07] [Server thread/INFO]: CupcakkeV issued server command: /TVYES</li>
+    <li>[18:24:07] [Server thread/INFO]: Navoko issued server command: /tv yes</li>
+    <li>[18:24:08] [Server thread/INFO]: [CapeCraft] Nessa_Wolfie joined the game</li>
+    <li>[18:24:08] [User Authenticator #884/INFO]: UUID of player Nessa_Wolfie is 5624dfe4-6a22-4ffb-bbf4-652e48203d6f</li>
+    <li>[18:24:09] [Server thread/INFO]: Nessa_Wolfie[/71.236.170.177:60885] logged in with entity id 5604939 at ([world]-2573.0, 73.0, 8256.0)</li>
+    <li>[18:24:10] [Server thread/INFO]: SmacK1776 issued server command: /tv yes</li>
+    <li>[18:24:11] [Server thread/INFO]: madxy_ issued server command: /wild</li>
+    <li>[18:24:12] [Server thread/INFO]: CupcakkeV issued server command: /tv yes</li>
+    <li>[18:24:13] [Server thread/INFO]: PLAYER » IntoHeaven: Nessa_Wolfie</li>
+    <li>[18:24:14] [Server thread/INFO]: _Bobster_ issued server command: /tv yes</li>
+    <li>[18:24:16] [Server thread/INFO]: PLAYER » IntoHeaven: hello</li>
+    <li>[18:24:19] [Server thread/INFO]: SalamanderCmndr issued server command: /home</li>
+    <li>[18:24:26] [Server thread/INFO]: TealKatt lost connection: Timed out</li>
+    <li>[18:24:26] [Server thread/INFO]: [CapeCraft] TealKatt left the game</li>
+  </ul>
   <div class="box mb-0">
     <div class="row">
       <div class="col">
